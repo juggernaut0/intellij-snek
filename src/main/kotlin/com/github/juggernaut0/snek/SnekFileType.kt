8bbb49285ca7279e22cbdee6c39cdf18aa97160d
Tom Warner <tomwamt@gmail.com>
2021-10-10 19:16:08 -0600

    Language and File Type

---
+++ src/main/kotlin/com/github/juggernaut0/snek/SnekFileType.kt
@@ -0,0 +1,11 @@
+package com.github.juggernaut0.snek
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+object SnekFileType: LanguageFileType(SnekLanguage) {
+    override fun getName(): String = "Snek File"
+    override fun getDescription(): String = "Snek language file"
+    override fun getDefaultExtension(): String = "snek"
+    override fun getIcon(): Icon? = null
+}
